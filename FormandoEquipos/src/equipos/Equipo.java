2365677982e6d4e94126551e0756b77527a53184
Agustin <aguscocciardi@gmail.com>
2019-05-09 10:17:20 -0300

    Se agrega la clase EquipoAfin

--- FormandoEquipos/src/equipos/Equipo.java
+++ FormandoEquipos/src/equipos/Equipo.java
@@ -8,100 +8,93 @@ import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class Equipo {
-	
-	private int afinidadMaxima;
-	private Integrante []integrantes;
+
+	private Integrante[] integrantes;
 	private int cantIntegrantes;
 	private int cantPreguntas;
-	
-	public Equipo(Scanner sc){
+	private EquipoAfin equipoAfin;
+
+	public Equipo(Scanner sc) {
 		this.cantPreguntas = sc.nextInt();
 		this.cantIntegrantes = sc.nextInt();
 		this.integrantes = new Integrante[cantIntegrantes];
-		for(int i=0; i<this.cantIntegrantes; i++){
+		for (int i = 0; i < this.cantIntegrantes; i++) {
 			this.integrantes[i] = new Integrante(sc.nextLine());
 		}
-		this.afinidadMaxima=-1;
-	}
-	
-	public void calcularAfinidadMaximaYFormarEquipo(PrintWriter pw){
-		this.afinidadMaxima = calcularAfinidadMaxima();
-		String equipo = formarEquipo(this.afinidadMaxima);
-		pw.println(this.afinidadMaxima);
-		pw.println(equipo);
-	}
-	
-	
-	
-	private String formarEquipo(int afinidadMaxima2) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
-	private int calcularAfinidadMaxima() {
-		int afinidadCalculada = 0;
+	public void grabarEnElArchivo(PrintWriter pw) {
+		calcularAfinidadMaximaYFormarEquipo();
+		pw.println(this.equipoAfin.getAfinidad());
+		pw.println(this.equipoAfin.getRespuestas());
+	}
+
+	private static int calcularAfinidad(int integrantes, int preguntas) {
+		return integrantes * (int) Math.pow(preguntas, 2);
+	}
+
+	private void calcularAfinidadMaximaYFormarEquipo() {
+		int afinidadCalculada;
 		int afinidadMaxima = 0;
-		boolean []evaluados = new boolean[this.cantIntegrantes];
-		//String []comb;
-		for(int i=1; i<this.cantPreguntas; i++){
-			for(int k= 0; k<this.cantIntegrantes; k++){
-				evaluados[k]=false;
+		boolean[] evaluados = new boolean[this.cantIntegrantes];
+		for (int i = 1; i < this.cantPreguntas; i++) {
+			for (int k = 0; k < this.cantIntegrantes; k++) {
+				evaluados[k] = false;
 			}
-			int j=0;
+			int j = 0;
 			int integrantes = 0;
-			int integrantesFalsos=this.cantIntegrantes;
-			int primerFalso=0;
-			boolean afinidadTomada=false;
+			int integrantesFalsos = this.cantIntegrantes;
+			int primerFalso = 0;
+			boolean afinidadTomada = false;
 			boolean primerFalsoHallado = false;
 			String palabra = "";
-			while(integrantesFalsos > 0 && j < this.cantIntegrantes){
-				if(afinidadTomada == false) {
-					j = primerFalso +1;
+			while (integrantesFalsos > 0 && j < this.cantIntegrantes) {
+				if (afinidadTomada == false) {
+					j = primerFalso + 1;
 					integrantes = 1;
 					palabra = this.integrantes[primerFalso].letraEnPosicion(i);
 					integrantesFalsos--;
 					evaluados[primerFalso] = true;
 					afinidadTomada = true;
 				}
-				if(evaluados[j] == false) {
-					if(this.integrantes[j].letraEnPosicion(i).equals(palabra)) {
+				if (evaluados[j] == false) {
+					if (this.integrantes[j].letraEnPosicion(i).equals(palabra)) {
 						integrantes++;
 						integrantesFalsos--;
 						evaluados[j] = true;
 					}
-				}
-				else {
-					if(primerFalsoHallado == false) {
+				} else {
+					if (primerFalsoHallado == false) {
 						primerFalso = j;
 						primerFalsoHallado = true;
 					}
 				}
-				
-				if(j+1 == this.cantIntegrantes) {
-					afinidadCalculada = integrantes * (int)Math.pow(i,2);
-					if(afinidadCalculada > afinidadMaxima) {
+
+				if (j + 1 == this.cantIntegrantes) {
+					afinidadCalculada = calcularAfinidad(integrantes, i);
+					if (afinidadCalculada > afinidadMaxima) {
 						afinidadMaxima = afinidadCalculada;
+						this.equipoAfin.setAfinidad(afinidadCalculada);
+						this.equipoAfin.setRespuestas(palabra);
 					}
-					if(integrantesFalsos > 0) {
-						j=0;
+					if (integrantesFalsos > 0) {
+						j = 0;
 						afinidadTomada = true;
 						primerFalsoHallado = false;
 					}
 				}
 			}
 		}
-		return afinidadMaxima;
 	}
 
 	public static void main(String[] args) throws IOException {
 		Scanner entrada = new Scanner(new FileReader("equipo.in"));
 		PrintWriter salida = new PrintWriter(new FileWriter("equipo.out"));
-		
+
 		Equipo equipo1 = new Equipo(entrada);
-		equipo1.calcularAfinidadMaximaYFormarEquipo(salida);
-		
+		equipo1.grabarEnElArchivo(salida);
+
 		entrada.close();
 		salida.close();
 	}
-	
 }
---
+++ FormandoEquipos/src/equipos/EquipoAfin.java
@@ -0,0 +1,29 @@
+package equipos;
+
+public class EquipoAfin {
+	
+	private int afinidad;
+	private String respuestas;
+	
+	public EquipoAfin() {
+		this.afinidad = 0;
+		this.respuestas = "";
+	}
+	
+	public void setAfinidad(int afinidad) {
+		this.afinidad = afinidad;
+	}
+	
+	public void setRespuestas(String respuestas) {
+		this.respuestas = respuestas;
+	}
+	
+	public int getAfinidad() {
+		return this.afinidad;
+	}
+	
+	public String getRespuestas() {
+		return this.respuestas;
+	}
+	
+}
