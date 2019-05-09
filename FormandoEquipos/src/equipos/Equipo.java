401abc3fdedcfe213ca1e7f34345ac28f4216200
Agustin <aguscocciardi@gmail.com>
2019-05-09 09:40:55 -0300

    CalcularAfinidadCompletada

--- FormandoEquipos/src/equipos/Equipo.java
+++ FormandoEquipos/src/equipos/Equipo.java
@@ -40,27 +40,57 @@ public class Equipo {
 
 	private int calcularAfinidadMaxima() {
 		int afinidadCalculada = 0;
-		int integrantes= 0;
-		//boolean []evaluados = new boolean[this.cantIntegrantes];
-		String []comb;
+		int afinidadMaxima = 0;
+		boolean []evaluados = new boolean[this.cantIntegrantes];
+		//String []comb;
 		for(int i=1; i<this.cantPreguntas; i++){
 			for(int k= 0; k<this.cantIntegrantes; k++){
-				//evaluados[k]=false;
+				evaluados[k]=false;
 			}
 			int j=0;
+			int integrantes = 0;
 			int integrantesFalsos=this.cantIntegrantes;
 			int primerFalso=0;
-			while(integrantesFalsos > 0){
+			boolean afinidadTomada=false;
+			boolean primerFalsoHallado = false;
+			String palabra = "";
+			while(integrantesFalsos > 0 && j < this.cantIntegrantes){
+				if(afinidadTomada == false) {
+					j = primerFalso +1;
+					integrantes = 1;
+					palabra = this.integrantes[primerFalso].letraEnPosicion(i);
+					integrantesFalsos--;
+					evaluados[primerFalso] = true;
+					afinidadTomada = true;
+				}
+				if(evaluados[j] == false) {
+					if(this.integrantes[j].letraEnPosicion(i).equals(palabra)) {
+						integrantes++;
+						integrantesFalsos--;
+						evaluados[j] = true;
+					}
+				}
+				else {
+					if(primerFalsoHallado == false) {
+						primerFalso = j;
+						primerFalsoHallado = true;
+					}
+				}
 				
+				if(j+1 == this.cantIntegrantes) {
+					afinidadCalculada = integrantes * (int)Math.pow(i,2);
+					if(afinidadCalculada > afinidadMaxima) {
+						afinidadMaxima = afinidadCalculada;
+					}
+					if(integrantesFalsos > 0) {
+						j=0;
+						afinidadTomada = true;
+						primerFalsoHallado = false;
+					}
+				}
 			}
-			
 		}
-		
-		
-		for (int i=0; i<this.cantPreguntas; i++){
-			for 
-		}
-		return 0;
+		return afinidadMaxima;
 	}
 
 	public static void main(String[] args) throws IOException {
